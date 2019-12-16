0bb88c51ca01e891c4348470710a371e1a479446
Blerina <blerina1.p@gmail.com>
2019-12-16 12:26:56 +0100

    Initial commit

---
+++ ga-metaboxes.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Gourmet Artist Metaboxes
+ * Plugin Uri:
+ * Description: Adds Metaboxes to the Gourmet Artistry Website
+ * Version: 1.0
+ * Author: Blerina
+ * Author Uri:
+ * License: GPL2
+ * license Uri: https://www.gnu.ord/licenses/gpl2-0.0html
+ */
