495f91d993802f08d5b73b110644e5552698d6e3
Blerina <blerina1.p@gmail.com>
2019-12-16 12:33:49 +0100

    Adding the metabox zone

--- ga-metaboxes.php
+++ ga-metaboxes.php
@@ -9,3 +9,12 @@
  * License: GPL2
  * license Uri: https://www.gnu.ord/licenses/gpl2-0.0html
  */
+
+  function ga_add_metaboxes(){
+        add_meta_box('ga_metaboxes', 'Gourmet Artistry Metaboxes', 'ga_metaboxes_container', 'recipes', 'normal', 'high', null);
+   }
+    add_action('add_meta_boxes', 'ga_add_metaboxes');
+
+  function ga_metaboxes_container(){
+       echo "hello world";
+   }
