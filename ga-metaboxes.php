3008dada0c44182a669908aa78df33d86d639cf8
Blerina <blerina1.p@gmail.com>
2019-12-16 12:46:30 +0100

    Adding Fields to our Metabox.

--- ga-metaboxes.php
+++ ga-metaboxes.php
@@ -15,6 +15,27 @@
    }
     add_action('add_meta_boxes', 'ga_add_metaboxes');
 
-  function ga_metaboxes_container(){
-       echo "hello world";
+  function ga_metaboxes_container($post){
+       wp_nonce_field(basename(__FILE__), "meta-box-nonce");
+     ?>
+    <div>
+        <label for="input-metaboxes">Calories:</label>
+        <input type="text" name="input-metaboxes" value="<?php echo get_post_meta($post->ID,"input-metabox", true);?>">
+        <br/>
+
+        <label for="textarea-metabox">Recipe short Description</label>
+        <textarea name="textarea-metabox"><?php echo get_post_meta($post->ID,"textarea-metabox", true);?></textarea>
+        <br/>
+
+        <label for="dropdown-metabox">Rating:</label>
+        <select name="dropdown-metabox">
+            <?php
+            $options = array(1,2,3,4,5);
+            foreach($options as $key => $value){
+                echo"<option value='{$value}'>{$value}</option>";
+            }
+            ?>
+        </select>
+    </div>
+<?php
    }
